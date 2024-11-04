efdb40a7638f77970bc2df6024be0d1c5c36a74e
Sherazad2005 <s.abdallah@lprs.fr>
2024-11-04 14:54:13 +0100

    Editer/Supp

--- src/controleur/TraitementForum.php
+++ src/controleur/TraitementForum.php
@@ -8,13 +8,11 @@ $utilisateur = new \Forum([
     "messages" =>$_POST['messages'],
     "canal" =>$_POST['canal']
 ]);
-<<<<<<< HEAD:src/controleur/TraitementForum.php
-
 $utilisateur->ajouterUnForum();
 
-=======
+
 $utilisateur->ajouterUnForum($_POST);
->>>>>>> 8d7f07b0dd5456de088a0254851c56c28be8837e:src/controller/TraitementForum.php
+
 
 
 
